b3570edd50e60f40ecddd0425cddb15084ff6acb
SheTroxel <smtroxel@gmail.com>
2023-02-25 23:11:33 -0500

    updates to schema and schema image

--- employee_db_schema.sql
+++ employee_db_schema.sql
@@ -1,69 +1,59 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/XNiqNk
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+--DROP TABLE IF EXISTS Employee;
+--DROP TABLE IF EXISTS Department_Manager;
+--DROP TABLE IF EXISTS Salaries;
+--DROP TABLE IF EXISTS Titles;
+--DROP TABLE IF EXISTS Department_Name;
+--DROP TABLE IF EXISTS Department_Employees;
 
--- schema for Employees_DB
+-- Create Table Schema for Pewlett Hackard Employees --------------------
 
-CREATE TABLE "Employee" (
-    "emp_no" int   NOT NULL,
-    "emp_title_id" int   NOT NULL,
-    "first_name" varchar   NOT NULL,
-    "last_name" varchar   NOT NULL,
-    "birth_data" date   NOT NULL,
-    "sex" varchar   NOT NULL,
-    "hire_date" date   NOT NULL,
-    CONSTRAINT "pk_Employee" PRIMARY KEY (
-        "emp_no"
-     )
+-- Create Table For Employee Information---------------------------------
+
+CREATE TABLE Employee (
+    emp_no int PRIMARY KEY NOT NULL,
+    emp_title_id varchar NOT NULL,
+    birth_date date NOT NULL,
+	first_name varchar NOT NULL,
+    last_name varchar NOT NULL,
+    sex varchar NOT NULL,
+    hire_date date NOT NULL
+  );
+-- ------------------------------------------------------------------------
+-- Create Table For Department Manager Info-------------------------------- 
+
+CREATE TABLE Department_Manager (
+    dept_no varchar NOT NULL,
+    emp_no int NOT NULL
 );
+-- -----------------------------------------------------------------------
+-- Create Table For Salary Info ------------------------------------------
 
-CREATE TABLE "Department_Manager" (
-    "dept_no" int   NOT NULL,
-    "emp_no" int   NOT NULL
-);
+CREATE TABLE Salaries (
+    emp_no int NOT NULL,
+    salary int NOT NULL
+	);
+	
+-- -----------------------------------------------------------------------
+-- Create Table For Titles -----------------------------------------------
 
-CREATE TABLE "Salaries" (
-    "emp_no" int   NOT NULL,
-    "salary" decimal   NOT NULL
-);
+CREATE TABLE Titles (
+    title_id varchar,
+    title varchar
+	);
 
-CREATE TABLE "Titles" (
-    "title_id" int   NOT NULL,
-    "title" varchar   NOT NULL,
-    CONSTRAINT "pk_Titles" PRIMARY KEY (
-        "title_id"
-     )
-);
+-- -----------------------------------------------------------------------
+-- Create Table by Department --------------------------------------------
 
-CREATE TABLE "Department_Employees" (
-    "dept_emp_no" composite(emp_no,dept_no)   NOT NULL,
-    "emp_no" int   NOT NULL,
-    "dept_no" int   NOT NULL
-);
+CREATE TABLE Department_Name (
+    dept_no varchar PRIMARY KEY NOT NULL,
+    dept_name varchar
+	);
 
-CREATE TABLE "Department_Name" (
-    "dept_no" int   NOT NULL,
-    "dept_name" varchar   NOT NULL,
-    CONSTRAINT "pk_Department_Name" PRIMARY KEY (
-        "dept_no"
-     )
-);
-
-ALTER TABLE "Employee" ADD CONSTRAINT "fk_Employee_emp_title_id" FOREIGN KEY("emp_title_id")
-REFERENCES "Titles" ("title_id");
-
-ALTER TABLE "Department_Manager" ADD CONSTRAINT "fk_Department_Manager_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Department_Name" ("dept_no");
-
-ALTER TABLE "Department_Manager" ADD CONSTRAINT "fk_Department_Manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employee" ("emp_no");
-
-ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employee" ("emp_no");
-
-ALTER TABLE "Department_Employees" ADD CONSTRAINT "fk_Department_Employees_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "Employee" ("emp_no");
-
-ALTER TABLE "Department_Employees" ADD CONSTRAINT "fk_Department_Employees_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Department_Name" ("dept_no");
+-- -----------------------------------------------------------------------
+-- -----------------------------------------------------------------------
+CREATE TABLE Department_Employees (
+	emp_no int,
+    dept_no varchar,
+    PRIMARY KEY (emp_no, dept_no)
+	);
 
